9b270b33d850547303db791b2fb4360c4dbf1936
Manfred Wuits <manfred@monochrom.at>
2019-12-09 11:11:22 +0100

    added build-files

---
+++ uba-demo-react/build/precache-manifest.39ed3e3fd2fcdac062f8eddc4168a119.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "938f36c41841b01118ee21c013a1f51e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9023155de615013da8ea",
+    "url": "/static/css/main.284f5a21.chunk.css"
+  },
+  {
+    "revision": "8ccdb9ec6380b200ba9c",
+    "url": "/static/js/2.c9500b4b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c9500b4b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9023155de615013da8ea",
+    "url": "/static/js/main.da2410f9.chunk.js"
+  },
+  {
+    "revision": "425523a19e751265a458",
+    "url": "/static/js/runtime-main.1ac22d8f.js"
+  }
+]);
